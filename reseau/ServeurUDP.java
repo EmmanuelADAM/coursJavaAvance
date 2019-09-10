0e452991f59230f3241d3c613ed3126e0887d09c
EmmanuelADAM <32464906+EmmanuelADAM@users.noreply.github.com>
2019-09-10 18:39:56 +0200

    Update ServeurUDP.java

--- reseau/ServeurUDP.java
+++ reseau/ServeurUDP.java
@@ -3,24 +3,23 @@ package reseau;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
-public class ServeurUDP{
-   public static void main(String arg[])
-   {
-      try(DatagramSocket socketServeur = new DatagramSocket(1234))
-      {
-         // Serveur
-         DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
-
-         int nbRecept = 0;
-         while(nbRecept<1000)
-         {
-            socketServeur.receive(packet);
-            byte[] b = packet.getData();
-            String ch = new String(b);
-            System.out.println("serveur -> j'ai reçu le paquet no " + nbRecept + ": " + ch);
-            nbRecept++;
-         }
-      }
-      catch(Exception e){System.err.println(e.getMessage());}
-   }
+public class ServeurUDP {
+    public static void main(String arg[]) throws IOException {
+        // creation d'une socket pour recevoir des paquets
+        try (DatagramSocket socketServeur = new DatagramSocket(1234)) {
+            
+            //modele de paquet (taille de 1024 byte)
+            DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+            
+            int nbRecept = 0;
+            while (nbRecept < 1000) {
+                //attente bloquante d'un paquet
+                socketServeur.receive(packet);
+                byte[] b = packet.getData();
+                String ch = new String(b);
+                System.out.println("serveur -> j'ai reçu le paquet no " + nbRecept + ": " + ch);
+                nbRecept++;
+            }
+        }
+    }
 }
