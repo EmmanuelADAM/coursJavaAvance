62287b8c478f4902ac8c6ea5c3cc3168af80c9e8
EmmanuelADAM <32464906+EmmanuelADAM@users.noreply.github.com>
2020-02-06 02:23:08 +0100

    Create ClasseSynchro.java

---
+++ processus/testSynchro/ClasseSynchro.java
@@ -0,0 +1,29 @@
+/**classe permettant de tester la synchronisation
+ * @author emmanuel adam
+ * */
+public class ClasseSynchro {
+
+	ClasseSynchro(){}
+	
+	/**tout processus entrant est bloqué 2 secondes. <br>
+	 * si i est impair, le processus est mis en pause<br>
+	 * si i est pair, le processus réveille tout processus en attente*/
+	public synchronized void  testPair(int i) 
+	{
+		System.out.println(Thread.currentThread().getName() + ", je suis entré");
+		try { Thread.sleep(2000); } 
+		catch (InterruptedException e1) {e1.printStackTrace(); }
+		if ((i % 2) != 0)
+		{
+			System.out.println("le nb n'est pas pair, je mets en attente");
+			try { wait(); } 
+		    catch (InterruptedException e) { e.printStackTrace(); }
+		}
+		else
+		{
+			System.out.println("le nb est pair");
+			notifyAll();
+		}
+		System.out.println(Thread.currentThread().getName() + " sort de la fonction");		
+	}
+}
